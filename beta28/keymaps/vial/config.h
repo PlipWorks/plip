47c02c2603ccb8aed10cbee31b766c8366d2f430
DarienJoso <darien.joso@gmail.com>
2022-02-10 22:31:13 -0800

    adding vial to beta28

---
+++ beta28/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x22, 0x39, 0xDC, 0xD9, 0x25, 0x2A, 0x45, 0xA1}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLUMNS { 0, 8 }
---
+++ beta28/keymaps/vial/rules.mk
@@ -0,0 +1,20 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = caterina
+
+EXTRAKEY_ENABLE 	= yes
+NKRO_ENABLE 		= yes
+COMBO_ENABLE = yes
+RGBLIGHT_ENABLE = yes
+BOOTMAGIC_ENABLE = yes
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
