ae6fddec4eb9574dea5d13702793b9a48f02c873
DarienJoso <darien.joso@gmail.com>
2022-07-17 03:01:40 -0700

    compiled

--- long_long_man/keymaps/via/keymap.c
+++ long_long_man/keymaps/via/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 						 KC_F1,   KC_F2,   KC_F3,   KC_HOME, 				  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,                    				 KC_P7,   KC_P8,   KC_P9,   _______, KC_PENT,
 						 KC_F4,   KC_F5,   KC_F6,   KC_PGUP, 				  KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, 				 KC_ENT,                     				 KC_P4,   KC_P5,   KC_P6,   _______, _______,
 						 KC_F7,   KC_F8,   KC_F9,   KC_PGDN, 				  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, 				 	KC_RSFT, 					 				 KC_UP,            KC_P1,   KC_P2,   KC_P3,   _______, KC_PPLS,
-						 KC_F10,  KC_F11,  KC_F12,  KC_END,	 				  KC_LCTL, KC_LGUI, KC_LALT, _______, KC_SPC, 				 				   KC_SPC, 					 KC_RALT, KC_RGUI, _______, KC_RCTL,  				LC_LEFT, KC_DOWN, KC_RGHT, _______, KC_P0,   K_PDOT,  _______, _______
+						 KC_F10,  KC_F11,  KC_F12,  KC_END,	 				  KC_LCTL, KC_LGUI, KC_LALT, _______, KC_SPC, 				 				   KC_SPC, 					 KC_RALT, KC_RGUI, _______, KC_RCTL,  				KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_P0,   KC_PDOT, _______, _______
   ),
 
 	[1] = LAYOUT(
