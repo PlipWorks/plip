006a7baccae23e16b4147e65e8a91b3295b7e08c
DarienJoso <darien.joso@gmail.com>
2022-07-17 04:02:10 -0700

    forgot to add labels for the layouts

--- long_long_man/keymaps/vial/config.h
+++ long_long_man/keymaps/vial/config.h
@@ -4,3 +4,13 @@
 
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 19 }
+
+// File "main_window.py", line 284, in on_device_selected
+// File "main_window.py", line 304, in rebuild
+// File "editor\keymap_editor.py", line 120, in rebuild
+// File "widgets\keyboard_widget.py", line 286, in set_keys
+// File "widgets\keyboard_widget.py", line 344, in update_layout
+// File "widgets\keyboard_widget.py", line 322, in place_widgets
+// File "editor\layout_editor.py", line 166, in get_choice
+//
+// IndexError: list index out of range
