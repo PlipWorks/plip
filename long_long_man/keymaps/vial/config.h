18edc5c706740be5a1ea45ab8758d590e0d85fb4
DarienJoso <darien.joso@gmail.com>
2022-07-18 11:02:02 -0700

    updated LLM hex file

--- long_long_man/keymaps/vial/config.h
+++ long_long_man/keymaps/vial/config.h
@@ -3,7 +3,7 @@
 #define VIAL_KEYBOARD_UID {0x3C, 0x4F, 0x28, 0x28, 0x50, 0xD8, 0x6A, 0xA6}
 
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 19 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 18 }
 
 // File "main_window.py", line 284, in on_device_selected
 // File "main_window.py", line 304, in rebuild
