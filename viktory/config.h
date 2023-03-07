5d89f22eb202849f613c07ad105028cb58b4ddb8
DarienJoso <darien.joso@gmail.com>
2023-03-06 23:58:40 -0800

    updating viktory files

--- viktory/config.h
+++ viktory/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 PLIP Works
+/* Copyright 2023 PLIP Works
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
