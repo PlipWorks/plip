547d5b184e9ddf98c5f5340271bfe42828c32ff9
PlipWorks <plip.works@gmail.com>
2024-12-09 12:11:55 -0800

    updating d1p files

--- d1p/config.h
+++ d1p/config.h
@@ -17,40 +17,4 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter
-As long as VID doesn't interfere with existing USB VID, then you're fine.*/
-#define VENDOR_ID       0x5057 // PW == PLIP Works
-#define PRODUCT_ID      0x0001
-#define DEVICE_VER      0x0000
-#define MANUFACTURER    PLIP Works
-#define PRODUCT         D1P
-
-/* key matrix size */
-#define MATRIX_ROWS 1
-#define MATRIX_COLS 1
-
-#define MATRIX_ROW_PINS { B7 }
-#define MATRIX_COL_PINS { D2 }
-
 #define RGB_DI_PIN C5
-#undef RGBLED_NUM
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-// #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD + 2
-#define RGBLIGHT_DEFAULT_VAL 0
-#define RGBLED_NUM 2
-
-/* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION COL2ROW
-
-/* For iOS compatibility */
-#define USB_MAX_POWER_CONSUMPTION 100
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-// #define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-// #define LOCKING_RESYNC_ENABLE
